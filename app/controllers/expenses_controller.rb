2c247923fc30b8e50673827ee1889fc249183ba1
Dimple Yuen <dimpleyuen@gmail.com>
2015-05-16 20:29:40 +0800

    create an expense

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -5,4 +5,19 @@ class ExpensesController < ApplicationController
     @expenses = Expense.order(created_at: :desc).includes(:transactions)
   end
 
+  def create
+    @expense = Expense.new(post_params)
+    if @expense.save
+      render :json => { message: "saved", status: true }
+    else
+      render :json => { message: "not saved", status: false }
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:expense).permit(:expense_name, :total_amount)
+  end
+
 end
